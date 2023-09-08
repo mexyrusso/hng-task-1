cd3dcd80c417f1b2d2a4daab3a899238223bd724
mexyrusso <emmaomolewa@gmail.com>
2023-09-08 16:13:43 +0100

    commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
   function getCurrentDay() {
       const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
       const today = new Date();
-      return days(today.getDay());
+      return days[today.getDay()];
   }
 
    // Function to get current UTC time in milliseconds
